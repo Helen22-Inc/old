37818eaf15c6f45f0341b4fb29553d0aae534bf6
Helen22-Inc <69580837+Helen22-Inc@users.noreply.github.com>
2020-08-18 01:27:41 +0300

    Update old.py

--- old.py
+++ old.py
@@ -18,13 +18,11 @@ async def on_ready():
     await client.change_presence(status=discord.Status.dnd,activity=discord.Game(f"Находится в доме престарелых | {len(client.get_guild(711118908081569793).members)}"))
     
 @client.command()
-async def se(message, kuda = None, otkuda_channel = None, otkuda_msg = None, txt_channel = None, txt_msg = None):
+async def se(message, kuda = None, otkuda_msg = None, txt_msg = None, fm1 = None, fm2 = None, fm3 = None, fm4 = None, fm5 = None):
     if message.author.id == 378559963494088707 or message.author.id == 414119169504575509:
         kuda = int(kuda.replace('<', '').replace('>', '').replace('#', ''))
-        otkuda_channel = int(otkuda_channel.replace('<', '').replace('>', '').replace('#', ''))
-        txt_channel = int(txt_channel.replace('<', '').replace('>', '').replace('#', ''))
-        description = await client.get_channel(txt_channel).fetch_message(int(txt_msg))
-        a = await client.get_channel(otkuda_channel).fetch_message(int(otkuda_msg))
+        description = await client.get_channel(743198152403714129).fetch_message(int(txt_msg))
+        a = await client.get_channel(742376455329284177).fetch_message(int(otkuda_msg))
 
         color, title = '000000', None
         for i in a.content.split('\n'):
@@ -46,18 +44,30 @@ async def se(message, kuda = None, otkuda_channel = None, otkuda_msg = None, txt
                     embed.set_footer(text=b[0])
             if b[0] == 'image':
                 embed.set_image(url=b[1][1::])
-
+        if not fm1 is None:
+            fo1 = await client.get_channel(743198152403714129).fetch_message(int(fm1))
+            embed.add_field(name = fo1.content.split('\n\n\n')[0], value = fo1.content.split('\n\n\n')[1], inline = False)
+        if not fm2 is None:
+            fo2 = await client.get_channel(743198152403714129).fetch_message(int(fm2))
+            embed.add_field(name = fo2.content.split('\n\n\n')[0], value = fo2.content.split('\n\n\n')[1], inline = False)
+        if not fm3 is None:
+            fo3 = await client.get_channel(743198152403714129).fetch_message(int(fm3))
+            embed.add_field(name = fo3.content.split('\n\n\n')[0], value = fo3.content.split('\n\n\n')[1], inline = False)
+        if not fm4 is None:
+            fo3 = await client.get_channel(743198152403714129).fetch_message(int(fm4))
+            embed.add_field(name = fo3.content.split('\n\n\n')[0], value = fo3.content.split('\n\n\n')[1], inline = False)
+        if not fm5 is None:
+            fo3 = await client.get_channel(743198152403714129).fetch_message(int(fm5))
+            embed.add_field(name = fo3.content.split('\n\n\n')[0], value = fo3.content.split('\n\n\n')[1], inline = False)
         await client.get_channel(kuda).send(embed=embed)
 
 @client.command()
-async def ee(message, channel_edit = None, channel_msg = None, otkuda_channel = None, otkuda_msg = None, txt_channel = None, txt_msg = None):
+async def ee(message, channel_edit = None, channel_msg = None, otkuda_msg = None, txt_msg = None, fm1 = None, fm2 = None, fm3 = None, fm4 = None, fm5 = None):
     if message.author.id == 378559963494088707 or message.author.id == 414119169504575509:
         channel_edit = int(channel_edit.replace('<', '').replace('>', '').replace('#', ''))
         msg = await client.get_channel(channel_edit).fetch_message(int(channel_msg))
-        otkuda_channel = int(otkuda_channel.replace('<', '').replace('>', '').replace('#', ''))
-        txt_channel = int(txt_channel.replace('<', '').replace('>', '').replace('#', ''))
-        description = await client.get_channel(txt_channel).fetch_message(int(txt_msg))
-        a = await client.get_channel(otkuda_channel).fetch_message(int(otkuda_msg))
+        description = await client.get_channel(743198152403714129).fetch_message(int(txt_msg))
+        a = await client.get_channel(742376455329284177).fetch_message(int(otkuda_msg))
 
         color, title = '000000', None
         for i in a.content.split('\n'):
@@ -77,6 +87,21 @@ async def ee(message, channel_edit = None, channel_msg = None, otkuda_channel = 
                     embed.set_footer(text=b[0], icon_url=b[1])
                 except:
                     embed.set_footer(text=b[0])
+        if not fm1 is None:
+            fo1 = await client.get_channel(743198152403714129).fetch_message(int(fm1))
+            embed.add_field(name = fo1.content.split('\n\n\n')[0], value = fo1.content.split('\n\n\n')[1], inline = False)
+        if not fm2 is None:
+            fo2 = await client.get_channel(743198152403714129).fetch_message(int(fm2))
+            embed.add_field(name = fo2.content.split('\n\n\n')[0], value = fo2.content.split('\n\n\n')[1], inline = False)
+        if not fm3 is None:
+            fo3 = await client.get_channel(743198152403714129).fetch_message(int(fm3))
+            embed.add_field(name = fo3.content.split('\n\n\n')[0], value = fo3.content.split('\n\n\n')[1], inline = False)
+        if not fm4 is None:
+            fo3 = await client.get_channel(743198152403714129).fetch_message(int(fm4))
+            embed.add_field(name = fo3.content.split('\n\n\n')[0], value = fo3.content.split('\n\n\n')[1], inline = False)
+        if not fm5 is None:
+            fo3 = await client.get_channel(743198152403714129).fetch_message(int(fm5))
+            embed.add_field(name = fo3.content.split('\n\n\n')[0], value = fo3.content.split('\n\n\n')[1], inline = False)
         await msg.edit(embed=embed)
         
 @client.command() 
